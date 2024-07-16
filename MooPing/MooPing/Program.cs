a659e6a20cd45b0482b5ab8d11c1170ad73bf0f2
sanyapoom.siri@gmail.com <sanyapoom.siri@gmail.com>
2024-07-16 15:26:16 -0700

    [Program.cs] add swaggerm carterm weboptimizer

--- MooPing/MooPing/Program.cs
+++ MooPing/MooPing/Program.cs
@@ -1,3 +1,4 @@
+using Carter;
 using MooPing.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,14 +8,25 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCarter();
 
+builder.Services.AddWebOptimizer(pipeline =>
+{
+    pipeline.AddCssBundle("/css/site.css", "scss/site.scss");
+    pipeline.MinifyJsFiles("js/**/*.js", "js/**/*.js");
+});
 #endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
@@ -23,10 +35,12 @@ else
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapCarter(); //Map Api
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
